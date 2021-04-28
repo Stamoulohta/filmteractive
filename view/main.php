a91809c173496f7cd1fa5452d192ba99ad5e26d7
George Stamoulis <g.a.stamoulis@gmail.com>
2021-04-29 01:02:27 +0300

    Ammends

--- view/main.php
+++ view/main.php
@@ -10,11 +10,13 @@
 
   <meta name="description" content="AψURDISM is an interactive web performance" />
   <meta name="keywords" content="stamoulohta, apsurdism, aψourdism, interactive, performance, filmteractive, opero" />
+
+  <meta property="fb:app_id" content="908041063322452" />
   <meta property="og:type" content="video.other" />
   <meta property="og:title" content="AψURDISM" />
   <meta property="og:description" content="An interactive web performance" />
   <meta property="og:url" content="https://opero.gr/apsurdism" />
-  <meta property="og:image" content="https://opero.gr/apsurdism/assets/share.jpg" />
+  <meta property="og:image" content="https://opero.gr/apsurdism/assets/facebook.jpg" />
 
   <link rel="manifest" href="<?= bust($manifest) ?>">
   <link rel="stylesheet" href="<?= bust($style) ?>">
